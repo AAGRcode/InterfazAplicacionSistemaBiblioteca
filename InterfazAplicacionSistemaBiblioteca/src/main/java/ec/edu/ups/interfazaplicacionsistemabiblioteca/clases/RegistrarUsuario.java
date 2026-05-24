089031df457a8f6e1c43f83d2e2fb934ab85380b
AAGRcode <axel.gonzalez.280907@gmail.com>
2026-05-24 17:01:32 -0500

    SEGUNDO COMMIT

--- InterfazAplicacionSistemaBiblioteca/src/main/java/ec/edu/ups/interfazaplicacionsistemabiblioteca/clases/RegistrarUsuario.java
+++ InterfazAplicacionSistemaBiblioteca/src/main/java/ec/edu/ups/interfazaplicacionsistemabiblioteca/clases/RegistrarUsuario.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ec.edu.ups.interfazaplicacionsistemabiblioteca.clases;
 
 /**
  *
- * @author USUARIO
+ * @author AAGR
  */
 public class RegistrarUsuario {
     
